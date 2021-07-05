aa702651e557678c321f4841253f7dfbd73aede6
Mateus Vespasiano de Castro <mateusvc1124@gmail.com>
2021-07-05 19:03:38 -0300

    Update

---
+++ Vscode/Lab 3 - Exercício C/programa5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    float s1, s2, s3;
+
+    printf("Digite o  segmento a: ");
+    scanf("%f", &s1);
+    printf("Digite o  segmento b: ");
+    scanf("%f", &s2);
+    printf("Digite o  segmento c: ");
+    scanf("%f", &s3);
+
+    if (s1 < (s2 + s3) && s2 < (s1 + s3) && s3 < (s2 + s1)){
+        if (s1 == s2 && s2 == s3)
+            printf("Os segmentos formam um triangulo Equilatero!");
+        else if (s1 == s2|| s1 == s3 || s2 == s3)
+            printf("O triangulo e Isoceles");
+        else
+            printf("O triangulo e Escaleno");
+    } else 
+        printf("Não podem formar um triangulo");
+}
