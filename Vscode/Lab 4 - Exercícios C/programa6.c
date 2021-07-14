10c93f654654876b12e4e173da837573ee7f4350
Mateus Vespasiano de Castro <mateusvc1124@gmail.com>
2021-07-14 18:02:04 -0300

    Multiplicação de Matrizes em C

--- Vscode/Lab 4 - Exercícios C/programa6.c
+++ Vscode/Lab 4 - Exercícios C/programa6.c
@@ -4,50 +4,87 @@ Mateus Vespasiano de Castro RA: 2248751
 */
 
 #include <stdio.h>
-#include <locale.h>
-int main(){
-setlocale(LC_ALL,"");
-int matrizA [3][4], matrizB [3][4], matrizC[3][4];
-int i,j;
-printf("Insira os valores para a matriz A 3 x 4: \n");
-for (i=0;i<3;i++){
-    for (j=0;j<4;j++){
-        printf("Linha [%d]: ", i);
-        printf("Coluna [%d]: ", j);
-        scanf("%d", &matrizA[i][j]);
-    }
+
+main() {
+
+
+int i, j, linhaA, colunaA, linhaB, colunaB, x;
+
+
+printf("\n Informe a quntidade de linhas da matriz A: ");
+scanf("%d",&linhaA);
+printf("\n Informe a quantidade de colunas da matriz A: ");
+scanf("%d",&colunaA);
+printf("\n Informe a quntidade de linhas da matriz B: ");
+scanf("%d",&linhaB);
+printf("\n Informe a quantidade de colunas da matriz B: ");
+scanf("%d",&colunaB);
+
+float matrizA[linhaA][colunaA], matrizB[linhaB][colunaB], matrizC[linhaA][colunaB], aux = 0;
+
+if(colunaA == linhaB) {
+
+	for(i = 0; i < linhaA; i++) {
+		for(j = 0; j < colunaA; j++) {
+			printf("\n\n Informe o valor da %d Linha e da %d Coluna da Matriz A: ", i+1, j+1);
+			scanf("%f", &matrizA[i][j]);
+		}
+	}
+    printf("\n");
+
+	for(i = 0; i < linhaB; i++) {        
+		for(j = 0; j < colunaB; j++) {
+			printf("\n\n Informe o valor da %d Linha e da %d Coluna da Matriz B: ", i+1, j+1);
+			scanf("%f", &matrizB[i][j]);
+		}
+	}
+    printf("\n");
+
+	for(i = 0; i < linhaA; i++) {
+        printf("[");     
+		for(j = 0; j < colunaA; j++) {
+			printf(" %0.f ", matrizA[i][j]);
+		}
+		printf("]\n");
+	}
+
+    printf("   *   ");
+    printf("\n");
+
+	for(i = 0; i < linhaB; i++) {
+        printf("[");
+		for(j = 0; j < colunaB; j++) {
+			printf(" %0.f ", matrizB[i][j]); 
+		}
+		printf("]\n");
+	}
+
+	for(i = 0; i < linhaA; i++) {
+		for(j = 0; j < colunaB; j++) {
+			
+			matrizC[i][j] = 0;
+			for(x = 0; x < linhaB; x++) {
+				aux +=  matrizA[i][x] * matrizB[x][j];
+			}
+
+			matrizC[i][j] = aux;
+			aux = 0;
+		}
+	}
+	
+    printf("    =  ");
+    printf("\n");
+
+	for(i = 0; i < linhaA; i++) {
+        printf("[");
+		for(j = 0; j < colunaB; j++) {
+			printf(" %0.f ", matrizC[i][j]);
+		}
+		printf("]\n");
+	}
+	printf("\n\n");
+} else {
+	printf("\n\n Nao tem como multiplicar as matrizes informadas por coluna A e diferente de B :(");
+    printf("\n");
 }
-printf("\nA matriz A 3x4: \n");
-for (i=0; i<3;i++){
-    printf("[");
-    for(j=0; j<4; j++){
-        printf(" %d ", matrizA[i][j]);
-    }
-    printf("]\n");
-}
-printf("\nA matriz B 3x4: \n");
-for (i=0;i<3;i++){
-    for (j=0;j<4;j++){
-        printf("Linha [%d]: ", i);
-        printf("Coluna [%d]: ", j);
-        scanf("%d", &matrizB[i][j]);
-    }
-}
-for (i=0; i<3;i++){
-    printf("[");
-    for(j=0; j<4; j++){
-        printf(" %d ", matrizB[i][j]);
-    }
-    printf("]\n");
-}
-printf("\nA matriz C 3x4: \n");
-for (i=0; i<3;i++){
-    printf("[");
-    for(j=0; j<4; j++){
-        matrizC [i][j] = matrizA[i][j] + matrizB [i][j];
-        printf(" %d ", matrizC[i][j]);
-    }
-    printf("]\n");
-}
-return (0);
 }
