182d0b0fa6f607ea9b57a7e5fd75df37a7472323
Mateus Vespasiano de Castro <mateusvc1124@gmail.com>
2021-07-19 11:24:13 -0300

    Update RGB code

--- Vscode/Lab 4 - Exercícios C/programa4.c
+++ Vscode/Lab 4 - Exercícios C/programa4.c
@@ -6,35 +6,51 @@ Mateus Vespasiano de Castro RA: 2248751
 */
 
 #include <stdio.h>
-#define MAX 128
+#define L 128
+#define C 128
+#define P 3
 
 int main(){
 
-    int rgb, pixel, m, n, b, c, cont=1;
-    int matriz[MAX][MAX][3];
+    int pixel[L][C][P];
+    int linha, coluna, rgb;
 
     
-    for (m=0; m<MAX; m++){
-        for (n=0; n<MAX; n++){
-            for (b=0; b<3;b++){
-                matriz[m][n][b] = cont;
-                cont++;
-                
+    for (linha=0; linha<L; linha++){
+        for (coluna=0; coluna<C; coluna++){
+            for (rgb=0; rgb<3;rgb++){
+                pixel[linha][coluna][rgb] = 0;
             }
             
         }
     }
-    for (m=0; m<MAX; m++){
-        for (n=0; n<MAX; n++){
-            printf("[");
-            for (b = 0; b<3; b++){
-                printf("%d\t", matriz[m][n][b]);
+
+    do{
+        printf("Escolha um píxel {128x128}, linha X coluna: <1x128>");
+        scanf("%dx%d", &linha, &coluna);
+        linha = linha - 1;
+        coluna = coluna - 1;
+        printf("Digite R: ");
+        scanf("%d", &pixel[linha][coluna][0]);
+        printf("Digite G: ");
+        scanf("%d", &pixel[linha][coluna][1]);
+        printf("Digite B: ");
+        scanf("%d", &pixel[linha][coluna][2]);
+        printf("Deseja digitar outro pixel? [1: YES, -1:NO]:");
+        scanf("%d", &linha);
+    } while (linha != -1);
+
+    for (linha=0; linha<L; linha++){
+        for (coluna=0; coluna<C; coluna++){
+            printf(" [ ");
+            for (rgb = 0; rgb<3; rgb++){
+                printf("%d ", pixel[linha][coluna][rgb]);
             }
             printf("]");
             
         }
         printf("\n");
     }
-
+    return 0;
 }
 
