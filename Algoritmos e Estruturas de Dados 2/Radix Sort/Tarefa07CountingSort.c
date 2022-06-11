8a36356c8d394b2f311887cdfa8419995dd80eb6
mateusvdcastro <mateusvc1124@gmail.com>
2022-06-11 14:18:23 -0300

    Update

---
+++ Algoritmos e Estruturas de Dados 2/Radix Sort/Tarefa07CountingSort.c
@@ -0,0 +1,117 @@
+//Eduardo Verissimo Faccio - 148859 - IA
+//Tarefa 7 - Palavras Ordenadas V 2.0
+
+#include <stdio.h>
+#include <string.h>
+
+//Valor para facilitar no Counting Sort
+#define ASCII 96
+
+typedef struct{
+    char palavra[21];
+} PALAVRAS;
+
+//Funcoes dadas pelo monitor
+void countingSort(PALAVRAS A[], PALAVRAS B[], int k, int n, int d);
+void radixSort(PALAVRAS A[], PALAVRAS B[], int k, int n, int d);
+
+int main(void){
+    int i, j, n, m, p, maiorPalavra = 0;
+
+    scanf("%d", &n);
+    PALAVRAS vetorA[n+1], vetorB[n+1];
+
+    //Inicio da lista e procurando o tamanho da maior palavra
+    for(i=1; i<n+1; i++){
+        scanf("%s", vetorA[i].palavra);
+        if(strlen(vetorA[i].palavra) > maiorPalavra){
+            maiorPalavra = strlen(vetorA[i].palavra);
+        }
+    }
+
+    //Retirar letras maiusculas
+    for(i=1; i<n+1; i++){
+        for(j=0; j<strlen(vetorA[i].palavra); j++){
+            if(vetorA[i].palavra[j] < 97){
+                vetorA[i].palavra[j] = vetorA[i].palavra[j] + 32;
+            }
+        }
+    }
+
+    //Adicionar espacos em branco e ponto final
+    for(i=1; i<n+1; i++){
+        strcat(vetorA[i].palavra, ".");
+        for(j=strlen(vetorA[i].palavra); j<maiorPalavra+1; j++){
+            strcat(vetorA[i].palavra, " ");
+        }
+        printf("%s\n", vetorA[i].palavra);
+    }
+    printf("%d\n", maiorPalavra);
+
+    radixSort(vetorA, vetorB, 27, n, maiorPalavra);
+
+    scanf("%d %d", &p, &m);
+    //Vetor Auxiliar para retirar o ponto final
+    PALAVRAS vetorAux[m];
+
+    //Mostrar palavras escolhidas sem o ponto final
+    for(i=0; i<m; i++){
+        for(j=0; j<strlen(vetorB[p+i].palavra); j++){
+            if(vetorB[p+i].palavra[j] == '.'){
+                strncpy(vetorAux[i].palavra, vetorB[p+i].palavra, j);
+                vetorAux[i].palavra[j] = '\0';
+                printf("%s\n", vetorAux[i].palavra);
+            }
+        }
+    }
+    return 0;
+}
+
+void countingSort(PALAVRAS A[], PALAVRAS B[], int k, int n, int d){
+    int C[k], i, j, v_ascii = ASCII;
+
+    for(i=0; i<k; i++){
+        C[i] = 0;
+    }
+
+    for(j = 1; j<n+1; j++){
+        if((A[j].palavra[d] != ' ') && (A[j].palavra[d] != '.')){
+            C[A[j].palavra[d] - v_ascii] = C[A[j].palavra[d] - v_ascii]+1;
+        }
+        else{
+            C[0] = C[0]+1;
+        }
+    }
+
+    for(i=1; i<k; i++){
+        C[i] = C[i] + C[i-1];
+    }
+
+    //Mostrar vetor C
+    printf("%d", C[0]);
+    for(i=1; i<27; i++){
+        printf(" %d", C[i]);
+    }
+    printf("\n");
+
+    for(j=n; j>0; j--){
+        if((A[j].palavra[d] != 32) && (A[j].palavra[d] != '.')){
+            strcpy(B[C[A[j].palavra[d] - v_ascii]].palavra, A[j].palavra);
+            C[A[j].palavra[d] - v_ascii] = C[A[j].palavra[d] - v_ascii] - 1;
+        }
+        else{
+            strcpy(B[C[0]].palavra, A[j].palavra);
+            C[0] = C[0] - 1;
+        }
+    }
+}
+
+void radixSort(PALAVRAS A[], PALAVRAS B[], int k, int n, int d){
+    int i, j;
+    for(i=d-1; i>=0; i--){
+        countingSort(A, B, k, n, i);
+        for(j=1; j<n+1; j++){
+            strcpy(A[j].palavra, B[j].palavra);
+        }
+    }
+}
--- Algoritmos e Estruturas de Dados 2/Radix Sort/main.c
+++ Algoritmos e Estruturas de Dados 2/Radix Sort/main.c
@@ -9,7 +9,8 @@ RA: 159505
 #include <ctype.h>
 
 // Converter as letras maiúsculas para minúsculas
-void transformaEmMinusculas(char **palavras, int n_palavras){
+void transformaEmMinusculas(char **palavras, int n_palavras)
+{
 
   char **palavras_min;
 
@@ -23,156 +24,154 @@ void transformaEmMinusculas(char **palavras, int n_palavras){
 }
 
 // Econtra a maior palavra e preenche com espaços em branco as outras palavras do vetor para que fiquem do mesmo tamanho da primeira
-void MaiorePreencheEspacos(char **palavras, int n_palavras, char *maior){
-
-  maior = malloc(21 * sizeof(char *));
+int MaiorePreencheEspacos(char **palavras, int n_palavras)
+{
+  int maior;
 
   for (int i = 0; i < n_palavras; i++)
   {
-    if (i == 0){
-      strcpy(maior, palavras[i]);
+    if (i == 0)
+    {
+      maior = strlen(palavras[i]);
     }
-    if (strlen(palavras[i]) > strlen(maior)){
-      strcpy(maior, palavras[i]);
+    if (strlen(palavras[i]) > maior)
+    {
+      maior = strlen(palavras[i]);
     }
   }
-  
+
   for (int i = 0; i < n_palavras; i++)
   {
-    for (int j = 0; j < strlen(maior); j++)
+    for (int j = strlen(palavras[i]); j < maior; j++)
     {
-      if (palavras[i][j] < 97 || palavras[i][j] > 122){
+      if (palavras[i][j] < 97 || palavras[i][j] > 122)
+      {
         palavras[i][j] = ' ';
       }
     }
   }
+
+  return maior;
 }
 
-void CountingSort(char **palavras, char **palavras_ordenadas, int k, int n_palavras){
+void CountingSort(char **A, char **B, int k, int n_palavras, int d)
+{
+
   int aux_B;
-  int *palavras_auxiliares; // vetor C
   int ascii; // A[j]
-  // palavras == vetor A
-  // palavras_ordenadas == vetor B
-  
-  palavras_auxiliares = (int *)calloc(k+1, sizeof(int));
-  
+  int C[k];
+
   // tamanho do vetor palavras
   int n = n_palavras;
-  
-  
+
   for (int i = 0; i < k; i++)
   {
-    palavras_auxiliares[i] = 0;
+    C[i] = 0;
   }
 
-  // coloca no vetor c a quantidade de ocorrências 
+  // coloca no vetor c a quantidade de ocorrências
   for (int i = 0; i < n; i++)
   {
-    for (int j = 0; j < 1; j++) // vai ver somente a primeira letra de cada palavra
+    // printf("\n[%d][%d]: %c ", i, d, palavras[i][d]);
+    if (A[i][d] <= 122 && A[i][d] >= 97)
     {
-      //printf("%c\n", palavras[i][j]);
-      if (palavras[i][j] <= 122 || palavras[i][j] >= 97){
-        ascii = palavras[i][j] - 96;  // ascii == A[j]
-        //printf("%d ", ascii);
-        palavras_auxiliares[ascii] = palavras_auxiliares[ascii] + 1;
-      }
+      ascii = A[i][d] - 96; // ascii == A[j]
+      C[ascii] = C[ascii] + 1;
     }
-  }
-  
-  /*
-  for (int i = 0; i < k; i++)
-  {
-    printf("%d %d ", i, palavras_auxiliares[i]);
-  } 
-  */
-  
-  for (int i = 0; i < k; i++)
-  {
-    palavras_auxiliares[i] = palavras_auxiliares[i] + palavras_auxiliares[i-1];
-    //printf("%d ", palavras_auxiliares[i]);
+    else
+    { // para caso seja espaço
+      C[0] = C[0] + 1;
+    }
   }
 
-  /*
+  for (int i = 1; i < k; i++)
+  {
+    C[i] = C[i] + C[i - 1]; // Soma préfixa
+  }
+
   for (int i = 0; i < k; i++)
   {
-    printf("%d ", palavras_auxiliares[i]);
-  } 
-  */
-  
-  for (int i = n-1; i >= 0; i--){
-    for (int j = 0; j < 1; j++)
-    {
-    
-      ascii = palavras[i][j] - 96;  // ascii == A[j]
-      //printf("%d ", ascii);
-      aux_B = palavras_auxiliares[ascii];
-      strcpy(palavras_ordenadas[aux_B], palavras[ascii]);
-      printf("%s ", palavras_ordenadas[aux_B]);
-      /*
-      palavras_auxiliares[ascii] = palavras_auxiliares[ascii] - 1;
-      printf("%d\n", aux_B);
-      */
-      //palavras_ordenadas[aux_B] = palavras[ascii];
-      
-    }
-    
+    printf("%d ", C[i]);
   }
-  /*
-  for (int i = 0; i < n; i++)
+
+  printf("\n");
+
+  for (int i = n - 1; i >= 0; i--)
   {
-    printf("%s ", palavras_ordenadas[i]);
-  } 
-  */
-  
+    if (A[i][d] <= 122 && A[i][d] >= 97)
+    {
+      ascii = A[i][d] - 96; // ascii == A[j]
+      aux_B = C[ascii] - 1;
+      B[aux_B] = A[i];
+      C[ascii] = C[ascii] - 1;
+    }
+    else
+    {
+      // caso seja espaço
+      B[C[0] - 1] = A[i];
+      C[0] = C[0] - 1;
+    }
+  }
+
+  for (int j = 0; j < n; j++)
+  {
+    A[j] = B[j];
+  }
 }
 
+void RadixSort(char **A, char **B, int k, int d, int n)
+{
+
+  for (int i = d - 1; i >= 0; i--)
+  {
+    CountingSort(A, B, 27, n, i);
+  }
+}
 
 int main(void)
 {
 
   // k são as 26 letras do alfabeto mais o /n
   int n_palavras, M, k = 27, d;
-  char **palavras, *maior_palavra;
-  char **palavras_ordenadas;
+  char **A;
+  char **B;
 
   scanf("%d\n", &n_palavras);
 
   // cria um vetor de ponteiros do tipo de char
-  palavras = malloc(n_palavras * sizeof(char *));
+  A = malloc(n_palavras * sizeof(char *));
 
   // cada posição do vetor aponta para outro vetor
   // 21 pois o limite máximo da palavra são 20 caracteres mais o /n
   for (int i = 0; i < n_palavras; i++)
   {
-    palavras[i] = malloc(21 * sizeof(char*));
-  }
-
-  palavras_ordenadas = malloc(n_palavras * sizeof(char *));
-  for (int i = 0; i < n_palavras; i++)
-  {
-    palavras_ordenadas[i] = malloc(21 * sizeof(char*));
+    A[i] = malloc(21 * sizeof(char *));
   }
 
   for (int i = 0; i < n_palavras; i++)
   {
-    scanf("%s\n", palavras[i]);
+    scanf("%s\n", A[i]);
   }
 
-  transformaEmMinusculas(palavras, n_palavras);
-  
+  B = malloc(n_palavras * sizeof(char *));
+
   for (int i = 0; i < n_palavras; i++)
   {
-    printf("%s.\n", palavras[i]);
-  } 
+    B[i] = malloc(21 * sizeof(char *));
+  }
 
-  MaiorePreencheEspacos(palavras, n_palavras, maior_palavra);
+  transformaEmMinusculas(A, n_palavras);
 
-  d = strlen(maior_palavra);
+  for (int i = 0; i < n_palavras; i++)
+  {
+    printf("%s.\n", A[i]);
+  }
+
+  d = MaiorePreencheEspacos(A, n_palavras);
 
   printf("%d\n", d);
-  
-  CountingSort(palavras, palavras_ordenadas, k, n_palavras);
+
+  RadixSort(A, B, k, d, n_palavras);
 
   int vet[2];
 
@@ -181,15 +180,26 @@ int main(void)
   for (int i = 0; i < n; i++)
   {
     scanf("%d", &vet[i]);
-  }  
-  
+  }
+
+  for (int i = vet[0] - 1; i < vet[0] + vet[1] - 1; i++)
+  {
+    printf("%s\n", B[i]);
+  }
+
   for (int i = 0; i < n_palavras; i++)
   {
-    free(palavras[i]);  // libera os vetores que os ponteiros indicam em cada
+    free(A[i]); // libera os vetores que os ponteiros indicam em cada
     // posição do vetor palavras
   }
-  free(palavras); // libera o vetor todo 
+  free(A); // libera o vetor todo
 
+  for (int i = 0; i < n_palavras; i++)
+  {
+    free(B[i]); // libera os vetores que os ponteiros indicam em cada
+    // posição do vetor palavras
+  }
+  free(B); // libera o vetor todo
 
   return 0;
 }
